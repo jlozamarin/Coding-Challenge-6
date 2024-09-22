e5938f9bb576b16a1bd21b8c50b574419ec95074
Jennifer Loza-Marin <jenniferlozamarin@usf.edu>
2024-09-21 22:44:25 -0400

    Create displayEmployeeShifts function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -7,8 +7,7 @@ let employees = [
     },
     {
         name: "Jesenia Chatman",
-        shifts: [{ day: "Wednesday", hours: 7 }, { day: "Thursday", hours: 8 }
-        ]
+        shifts: [{ day: "Wednesday", hours: 7 }, { day: "Thursday", hours: 8 }]
     },
     {
         name: "Isabella Moore",
@@ -20,3 +19,15 @@ let employees = [
     }
 ];
 console.log(employees);
+
+// Task 2: Create a Function to Display Employee Shift Details
+function displayEmployeeShifts(employee) {
+    console.log(`Display shifts for ${employee.name}:`);
+    employee.shifts.forEach(shift => {
+        console.log(`Day: ${shift.day}, Hours: ${shift.hours}`);
+    });
+}
+displayEmployeeShifts(employees[0]); // Shows shifts for Brianna Deaubler
+displayEmployeeShifts(employees[1]); // Shows shifts for Jesenia Chatman
+displayEmployeeShifts(employees[2]); // Shows shifts for Isabella Moore
+displayEmployeeShifts(employees[3]); // Shows shifts for Jennifer Loza
