0a078516d4cb6ee07f1aacf4f9a046610062547f
Jennifer Loza-Marin <jenniferlozamarin@usf.edu>
2024-09-21 22:12:51 -0400

    Initialize employees with shift schedules

---
+++ employee_shift_management.js
@@ -0,0 +1,22 @@
+// Task 1: Create an Employees Array of Employee Objects
+// Notes: This shows a list of employees and their shift schedules
+let employees = [
+    {
+        name: "Brianna Deaubler",
+        shifts: [{ day: "Tuesday", hours: 8 }, {day: "Wednesday", hours: 8 }]
+    },
+    {
+        name: "Jesenia Chatman",
+        shifts: [{ day: "Wednesday", hours: 7 }, { day: "Thursday", hours: 8 }
+        ]
+    },
+    {
+        name: "Isabella Moore",
+        shifts: [{ day: "Monday", hours: 8 }, { day: "Friday", hours: 6 }]
+    },
+    {
+        name: "Jennifer Loza",
+        shifts: [{ day: "Tuesday", hours: 7 }, { day: "Friday", hours: 7 }]
+    }
+];
+console.log(employees);
